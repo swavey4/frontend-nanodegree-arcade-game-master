b9a9a184965bd1a756469a88b034295396f6d746
Gibbor Munyama <gibbormunyama@gmail.com>
2018-07-16 21:32:21 +0100

    feat: Sets the Enemy initial speed and location

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(x,y,speed) {
+    // Variables applied to each of our instances go here
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
